097bb66967a92b46f67013f72e04dd37170937d9
christian <christian.acosta@neoris.com>
2022-09-22 23:46:34 -0600

    se sube

---
+++ edcurso-go/02/01variablesyconstantes/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	nombre, nombre2, apellidoPaterno, apellidoMaterno := "Mateo", "Francisco", "Acosta", "Berrelleza"
+	apellidoMaterno = "Garcia1"
+	fmt.Println(nombre, nombre2, apellidoPaterno, apellidoMaterno)
+}
